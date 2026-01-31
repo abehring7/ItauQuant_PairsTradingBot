1a9d1c2e3ee5ac680315ff15fab7374a7e47f327
abehring7 <behringarthur@gmail.com>
2026-01-31 07:49:58 +0000

    atualizacao codigo

--- pairs_arb.py
+++ pairs_arb.py
@@ -80,184 +80,158 @@ def cointegration(): ## abrir o coint_ativos fazer os a coint pegar os residuos 
         print(f"erro : {e}")
     return stationary_df        ## df retornando quais sao os papeis estacionarios (pvalue e coint(pvalue))
 
-betas = []
-alphas = []
-stock1 = []
-stock2 = []
-def lin_regress_result():
-
-
-    base_path = Path("excel_folder")
-    camino = base_path / "close_cointegravel.xlsx"
-    path = base_path / "coint_result2.xlsx"
-
-
-    df_cointegravel = pd.read_excel(path)
-    df_close_coint = pd.read_excel(camino,index_col=0, parse_dates=True)
-
-
-    for index, row in df_cointegravel.iterrows(): ## percorre o df por linha/index
-        s1_name = row["stock1"]                   ## pega o nome do primeiro papel por row["stock1"]
-        s2_name = row["stock2"]                   ## mesma coisa soq com o papel2  
-        
-        
-
-        precos_stock1 = df_close_coint[s1_name]  ## acha o preco pegando o nome do ativo(acima) na coluna do close_coint
-        precos_stock2 = df_close_coint[s2_name]  ## mesma coisa, 
-        
-        precos_stock1 = np.log(precos_stock1)           ## transforma em array e escala log para nao quebrar o alpha
-        precos_stock2 = np.log(precos_stock2)
-        
-
-        beta, alpha, _, _, _ = stats.linregress(precos_stock1, precos_stock2)
-        betas.append(beta)
-        alphas.append(alpha)
-        stock1.append(s1_name)
-        stock2.append(s2_name)
-    
-    caminho = base_path / "linregress_resultado.xlsx"
-    df_linregress = pd.DataFrame({"stock1":stock1,         ##  alinha os elementos correspondentes a suas colunas
-                                  "stock2":stock2,
-                                  "alpha":alphas,
-                                  "beta":betas})
-    return df_linregress
 
 ## ---------------------------------------------------------------------------------------------------
 
-def spread_zcore():
-    base_path = Path("excel_folder")
+def zscore_signals_df():                ## calculo do spread/zscore seguido dos sinais por beta e alpha dinamicos
+    base_path = Path("excels_folder")
     arquivo_coint = pd.read_excel(
-    base_path / "coint_result2.xlsx"
+        base_path / "coint_result2.xlsx"
     )
-    a = pd.unique(arquivo_coint["stock1"].tolist() + arquivo_coint["stock2"].tolist())  ## acoes unicas nas duas colunas
+    a = pd.unique(arquivo_coint["stock1"].tolist() + arquivo_coint["stock2"].tolist())
     close = pd.read_excel(
-    base_path / "close_papeis.xlsx",
-    index_col=0,
-    parse_dates=True
+        base_path / "close_papeis.xlsx",
+        index_col=0,
+        parse_dates=True
     )
     close_filtrado = close[a]
+    quant_metrics = pd.DataFrame(index=close.index)
+    z_scoredf = pd.DataFrame(index=close.index) 
+    for index, row in arquivo_coint.iterrows():
+        papel1_name = row["stock1"]
+        papel2_name = row["stock2"]
+        preco1 = np.log(close_filtrado[papel1_name])
+        preco2 = np.log(close_filtrado[papel2_name])
 
-    df_linregress = df_linregress()
+        preco1_mean = preco1.rolling(window=63, min_periods=15).mean()
+        preco2_mean = preco2.rolling(window=63, min_periods=15).mean()
 
-    z_scoredf = pd.DataFrame(index=close.index)
-    for index, row in df_linregress.iterrows():
-        beta = row["betas"]
-        alpha = row["alphas"]
-        p1_name = row["papel1"]
-        p2_name = row["papel2"]
-        spread = np.log(close_filtrado[p2_name])-(np.log(close_filtrado[p1_name])*beta_dyn+alpha)
-        z_score = (spread-spread.mean()) / np.std(spread)
-        par = f"{p1_name}_{p2_name}"
-        z_scoredf[par] = z_score
+        rolling_cov = preco2.rolling(window=63, min_periods=15).cov(preco1)
+        rolling_var = preco1.rolling(window=63, min_periods=15).var()
+        rolling_beta = rolling_cov/rolling_var
 
+        beta_travado = [np.nan] * len(rolling_beta)
+        ultimo_beta = rolling_beta.dropna().iloc[0]
 
-def signals_df():
-    base_path = Path("excel_folder")
-    z_scoredf = pd.read_excel(base_path / "z_scoredf.xlsx", index_col=0, parse_dates=True)
-    signal_df = pd.DataFrame(index=z_scoredf.index)
-    for pairs in z_scoredf.columns:         
-        conditions = [                      ## [lista_(matematica_bool)_lista]
-            (z_scoredf[pairs]<-2),
-            (z_scoredf[pairs]>2)
-        ]
-        choices = [1, -1]
-        signal_df[f"{pairs}_signal"] = np.select(   ## como sao 3 condicoes(compra,venda,fazer nada) o np.where fica rui, assim usa-se select
-            conditions,
-            choices,
-            default=0
-        )
-    signal_df.to_excel(base_path/"signals_df.xlsx")
+        for i, beta in enumerate(rolling_beta): ## importante
+            if pd.isna(beta):
+                beta_travado[i] = ultimo_beta
+                continue
+            if abs((beta-ultimo_beta)/ abs(ultimo_beta)) >=0.07:
+                ultimo_beta = beta
+            beta_travado[i] = ultimo_beta
 
+        beta_travado = pd.Series(beta_travado, index=close_filtrado.index)
 
-
-## Funcoes estatisticas e criação do df z_score, sinais
-## ----------------------------------
-def backtest():  
-    base_path = Path("excel_folder")
-
-close_df = pd.read_excel(
-    base_path / "close_papeis.xlsx",
-    index_col=0,
-    parse_dates=True
-)                                                                  ## inicializacao de dfs
-coint_result = pd.read_excel(base_path/"coint_result2.xlsx")
-z_scoredf = pd.read_excel(
-    base_path/"z_scoredf.xlsx",
-    index_col=0,
-    parse_dates=True                     
-)
-
-size_df = pd.read_excel(
-    base_path/"signals_df.xlsx",
-    index_col=0,
-    parse_dates=True
-)
-backtest_df = pd.DataFrame()
-benchmark_df = yf.download("^BVSP", period="5y")["Close"]
-
-benchmark_df = benchmark_df.loc["2021-01-26":"2026-01-15"]
-benchmark_df = benchmark_df.pct_change()
-benchmark_df = benchmark_df.reindex(close_df.index)
-benchmark_df = benchmark_df.squeeze()           ## remove dimensoes a coluna (redundante)
-
-unic_tickers = pd.unique(coint_result["stock1"].to_list() + coint_result["stock2"].tolist())
-unic_tickers = unic_tickers.tolist()
-open_df = yf.download(unic_tickers, period="5y")["Open"] ## baixa o df de precos de abertura com base nos tickers unicos
-open_df = open_df.loc["2021-01-26":"2026-01-15"]
-open_df = open_df.reindex(close_df.index)
-open_df = open_df.interpolate(method="time", axis=0, limit_direction="backward", limit_area="outside")   ## interpola os NANS com valores aceitaveis
-beta_dinamico = pd.read_excel(base_path/"alpha_beta.xlsx", index_col=0, parse_dates=True)   ## inicializa os betas para calcular o notional
-
-## ------------------------------------
-## inicializaco e transformacao de dfs 
-## ------------------------------------
-
-for pair in z_scoredf.columns:
-    papel1, papel2 = pair.split("_")        ## tira o "_" e assinala cada variavel no lugar dela
-    sinal = size_df[f"{pair}_signal"]
-
-    beta_dyn = beta_dinamico[f"{pair}_beta"].clip(-3, 3)
-    
-    fator_nor = abs(beta_dyn).fillna(0)+1
-    
-    
-    signals_split = pd.DataFrame({                        ## cria a cada loop o df balanceado pelo beta para cada par
-        papel1:(-sinal*beta_dyn) / fator_nor,
-        papel2:sinal / fator_nor
-    }, index=close_df.index) ## assinala di
-    signals_split = signals_split.shift(1).fillna(0).clip(-1,1) ## evita lookaheadbias preenche os nan se tiver e resitringe a posivcao para -1,1
+        rolling_alpha = preco2_mean - (rolling_beta*preco1_mean)
         
-    portif = vbt.Portfolio.from_orders(
-        close=close_df[[papel1, papel2]],
-        size=signals_split[[papel1, papel2]],
-        price=open_df[[papel1, papel2]],
-        val_price=open_df[[papel1, papel2]],             ## da o notional da ordem, com base no sinal
-        freq="d",
-        size_type="targetpercent",
-        init_cash=10000,
-        direction="both",
-        fees = 0.0003,
-        slippage = 0.0005,
-        cash_sharing=True                                 ## permite o financiamento do long pelo short
+        spread = preco2 - (preco1*beta_travado+rolling_alpha)
+        z_score = (spread-spread.rolling(window=20, min_periods=15).mean()) / spread.rolling(window=20, min_periods=15).std()
+        par_name = f"{papel1_name}_{papel2_name}"
+        z_scoredf[par_name] = z_score
+        quant_metrics[f"{par_name}_beta"] = beta_travado
+        quant_metrics[f"{par_name}_alpha"] = rolling_alpha
+        print(quant_metrics[f"{par_name}_beta"])  
+
+    z_scoredf = z_scoredf.dropna()
+    z_scoredf.to_excel(base_path / "z_scoredf.xlsx")
+    quant_metrics = quant_metrics.dropna()
+    ##quant_metrics.to_excel(base_path / "alpha_beta.xlsx")
+    
+
+
+    signals_df = pd.DataFrame(index=z_scoredf.index)
+    for pair in z_scoredf:
+        conditions = [(z_scoredf[pair]<-1.5),
+                       (z_scoredf[pair]>1.5)]
+        choices = [1,-1]
+        signals_df[f"{pair}_signal"] = np.select(conditions, choices, default=0)
+    ##signals_df.to_excel(base_path / "signals_df.xlsx")
+
+
+
+
+## ----------------------------------
+def backtest():  ## backtest com base nos sinais
+    base_path = Path("excel_folder")
+
+    close_df = pd.read_excel(
+        base_path / "close_papeis.xlsx",
+        index_col=0,
+        parse_dates=True
+    )                                                                  ## inicializacao de dfs
+    coint_result = pd.read_excel(base_path/"coint_result2.xlsx")
+    z_scoredf = pd.read_excel(
+        base_path/"z_scoredf.xlsx",
+        index_col=0,
+        parse_dates=True                     
     )
-    estatisticas = portif.stats(                          ## determina o benchmark e mostra os valores do benchmark
-        settings=dict(
-            benchmark_rets=benchmark_df,
-            verbose=True
+
+    size_df = pd.read_excel(
+        base_path/"signals_df.xlsx",
+        index_col=0,
+        parse_dates=True
+    )
+    backtest_df = pd.DataFrame()
+    benchmark_df = yf.download("^BVSP", period="5y")["Close"]
+
+    benchmark_df = benchmark_df.loc["2021-01-26":"2026-01-15"]
+    benchmark_df = benchmark_df.pct_change()
+    benchmark_df = benchmark_df.reindex(close_df.index)
+    benchmark_df = benchmark_df.squeeze()           ## remove dimensoes a coluna (redundante)
+
+    unic_tickers = pd.unique(coint_result["stock1"].to_list() + coint_result["stock2"].tolist())
+    unic_tickers = unic_tickers.tolist()
+    open_df = yf.download(unic_tickers, period="5y")["Open"] ## baixa o df de precos de abertura com base nos tickers unicos
+    open_df = open_df.loc["2021-01-26":"2026-01-15"]
+    open_df = open_df.reindex(close_df.index)
+    open_df = open_df.interpolate(method="time", axis=0, limit_direction="backward", limit_area="outside")   ## interpola os NANS com valores aceitaveis
+    beta_dinamico = pd.read_excel(base_path/"alpha_beta.xlsx", index_col=0, parse_dates=True)   ## inicializa os betas para calcular o notional
+
+    ## ------------------------------------
+    ## inicializaco e transformacao de dfs 
+    ## ------------------------------------
+
+    for pair in z_scoredf.columns:
+        papel1, papel2 = pair.split("_")        ## tira o "_" e assinala cada variavel no lugar dela
+        sinal = size_df[f"{pair}_signal"]
+
+        beta_dyn = beta_dinamico[f"{pair}_beta"].clip(-3, 3)
+        
+        fator_nor = abs(beta_dyn).fillna(0)+1   ## fato de normalizacao eh o valor absoluto do beta + 1 que seria o peso fixo do papel2(independente)
+        
+        
+        signals_split = pd.DataFrame({          ## cria a cada loop o df balanceado pelo beta para cada par
+            papel1:(-sinal*beta_dyn) / fator_nor,
+            papel2:sinal / fator_nor
+        }, index=close_df.index) ## assinala di
+        signals_split = signals_split.shift(1).fillna(0).clip(-1,1) ## evita lookaheadbias preenche os nan se tiver e resitringe a posivcao para -1,1
+            
+        portif = vbt.Portfolio.from_orders(
+            close=close_df[[papel1, papel2]],
+            size=signals_split[[papel1, papel2]],
+            price=open_df[[papel1, papel2]],
+            val_price=open_df[[papel1, papel2]],             ## da o notional da ordem, com base no sinal
+            freq="d",
+            size_type="targetpercent",
+            init_cash=10000,
+            direction="both",
+            fees = 0.0003,
+            slippage = 0.0005,
+            cash_sharing=True                                 ## permite o financiamento do long pelo short
         )
-    )
-    backtest_df[pair] = estatisticas
+        estatisticas = portif.stats(                          ## determina o benchmark e mostra os valores do benchmark
+            settings=dict(
+                benchmark_rets=benchmark_df,
+                verbose=True
+            )
+        )
+        backtest_df[pair] = estatisticas
 
-print(backtest_df)
-
+    print(backtest_df)
 
 def main():
     backtest_df = backtest()
 
 if __name__ == "__main__":
     main()
-
-
-
-## git
-
